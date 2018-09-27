cbeb9e203ad5e8bf06f14d09eaa67ebddd629285
Erwin Dondorp <saltgui@dondorp.com>
2018-09-28 01:54:53 +0200

    match new code structure

--- saltgui/static/scripts/routes/pillars.js
+++ saltgui/static/scripts/routes/pillars.js
@@ -28,25 +28,6 @@ class PillarsRoute extends PageRoute {
     });
   }
 
-  _updateMinions(data) {
-    const minions = data.return[0];
-
-    const list = this.getPageElement().querySelector('#minions');
-    const hostnames = Object.keys(minions).sort();
-
-    for(let i = 0; i < hostnames.length; i++) {
-      const minion_info = minions[hostnames[i]];
-
-      // minions can be offline, then the info will be false
-      if (minion_info === false) {
-        this._updateOfflineMinion(list, hostnames[i]);
-      } else {
-        const minion = minions[hostnames[i]];
-        this._updateMinion(list, hostnames[i], minion);
-      }
-    }
-  }
-
   _updateKeys(data) {
     const keys = data.return;
 
@@ -61,47 +42,17 @@ class PillarsRoute extends PageRoute {
     if(this.keysLoaded && this.jobsLoaded) this.resolvePromise();
   }
 
-  _updateOfflineMinion(container, hostname) {
-    let element = document.getElementById(hostname);
-    if(element == null) {
-      // offline minion not found on screen...
-      // construct a basic element that can be updated here
-      element = document.createElement('li');
-      element.id = hostname;
-      container.appendChild(element);
-    }
-    while(element.firstChild) {
-      element.removeChild(element.firstChild);
-    }
+  _updateMinion(container, minion, hostname) {
 
-    element.appendChild(Route._createDiv("hostname", hostname));
-
-    const offline = Route._createDiv("offline", "offline");
-    offline.id = "status";
-
-    element.appendChild(offline);
-  }
-
-  _updateMinion(container, hostname, minion) {
-    const cnt = Object.keys(minion).length;
-
-    let element = document.getElementById(hostname);
-    if(element == null) {
-      // offline minion not found on screen...
-      // construct a basic element that can be updated here
-      element = document.createElement('li');
-      element.id = hostname;
-      container.appendChild(element);
-    }
-    while(element.firstChild) {
-      element.removeChild(element.firstChild);
-    }
+    const element = this._getElement(container, hostname);
 
     element.appendChild(Route._createDiv("hostname", hostname));
 
     const status = Route._createDiv("status", "accepted");
-    element.appendChild(status);
     status.classList.add("accepted");
+    element.appendChild(status);
+
+    const cnt = Object.keys(minion).length;
 
     let pillarInfoText;
     switch(cnt){
@@ -125,65 +76,10 @@ class PillarsRoute extends PageRoute {
     }
   }
 
-  _addMinion(container, hostname) {
-
-    let element = document.getElementById(hostname);
-    if(element != null) {
-      // minion already on screen...
-      return;
-    }
-
-    element = document.createElement('li');
-    element.id = hostname;
-
-    element.appendChild(Route._createDiv("hostname", hostname));
-
-    element.appendChild(Route._createDiv("os", "loading..."));
-
-    container.appendChild(element);
-  }
-
-  _updateJobs(data) {
-    const jobContainer = document.querySelector("#page_pillars .jobs");
-    jobContainer.innerHTML = "";
-    const jobs = this._jobsToArray(data.return[0]);
-    this._sortJobs(jobs);
-
-    //Add seven most recent jobs
-    let shown = 0;
-    let i = 0;
-    while(shown < 7 && jobs[i] !== undefined) {
-      const job = jobs[i];
-      i = i + 1;
-      if(job.Function === "saltutil.find_job") continue;
-      if(job.Function === "pillar.items") continue;
-      if(job.Function === "wheel.key.list_all") continue;
-      if(job.Function === "runner.jobs.list_jobs") continue;
-
-      this._addJob(jobContainer, job);
-      shown = shown + 1;
-    }
-    this.jobsLoaded = true;
-    if(this.keysLoaded && this.jobsLoaded) this.resolvePromise();
-  }
-
-  _addJob(container, job) {
-    const element = document.createElement('li');
-    element.id = job.id;
-
-    element.appendChild(Route._createDiv("function", job.Function));
-    element.appendChild(Route._createDiv("target", job.Target));
-    element.appendChild(Route._createDiv("time", job.StartTime));
-    container.appendChild(element);
-    element.addEventListener('click', this._createJobListener(job.id));
-  }
-
   _showPillars(evt, hostname, minion) {
 
     const title = document.getElementById("pillars_title");
     title.innerText = "Pillars on " + hostname;
-
-    const menu = new DropDownMenu(title);
 
     const container = document.getElementById("pillars_list");
 
@@ -272,47 +168,5 @@ class PillarsRoute extends PageRoute {
     pillarsContainer.style.display = "block";
     const jobContainer = document.querySelector("#page_pillars .job-list");
     jobContainer.style.display = "none";
-  }
-
-  _createJobListener(id) {
-    const router = this.router;
-    return function() {
-      router.goTo("/job?id=" + id);
-    };
-  }
-
-  _jobsToArray(jobs) {
-    const keys = Object.keys(jobs);
-    const newArray = [];
-
-    for(let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      const job = jobs[key];
-      job.id = key;
-      newArray.push(job);
-    }
-
-    return newArray;
-  }
-
-  _sortJobs(jobs) {
-    jobs.sort(function(a, b){
-      // The id is already a integer value based on the date, let's use
-      // it to sort the jobs
-      if (a.id < b.id) return 1;
-      if (a.id > b.id) return -1;
-      return 0;
-    });
-  }
-
-  _copyAddress(evt) {
-    const target = evt.target;
-    const selection = window.getSelection();
-    const range = document.createRange();
-
-    range.selectNodeContents(target);
-    selection.removeAllRanges();
-    selection.addRange(range);
-    document.execCommand("copy");
   }
 }
